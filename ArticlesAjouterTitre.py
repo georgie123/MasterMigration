183a73996b862a64f958726e2266a2b35794e1bb
georgie123 <georges.hinot@gmail.com>
2023-09-24 19:28:18 +0200

    Standard update

--- ArticlesAjouterTitre.py
+++ ArticlesAjouterTitre.py
@@ -22,19 +22,38 @@ for my_poi in root.findall('Tmp_extract_alias_joo_content'):
     xml_title = my_poi.find('title').text
     xml_alias = my_poi.find('alias').text
 
-    print(xml_title)
+    if xml_title != 'Test georges' \
+            and xml_title != 'Test cours m2 georges' \
+            and xml_title != 'Test georges4' \
+            and xml_title != 'Logo (New)' \
+            and xml_title != 'Présentation générale' \
+            and xml_title != 'Options d&apos;édition' \
+            and xml_title != 'Options d\'édition' \
+            and xml_title != 'Précisions' \
+            and xml_title != 'Bienvenue' \
+            and xml_title != 'IMG1' \
+            and xml_title != 'IMG2' \
+            and xml_title != 'IMG3' \
+            and xml_title != 'Présentation Actualités' \
+            and xml_title != 'Présentation Actualités 2' \
+            and xml_title != 'Master Géomatique' \
+            and xml_title != 'Expertise numérique et territoriale' \
+            and xml_title != 'Formation en alternance' \
+            and xml_title != 'XXXXXXXXX':
 
-    # browser.maximize_window()
-    # browser.get('https://joomla4.master-geomatique.org/administrator/index.php?option=com_content&view=article&layout=edit')
-    browser.get('https://j4binv.master-geomatique.org/administrator/index.php?option=com_content&view=article&layout=edit')
+        print(xml_title)
 
-    # CHAMPS CLASSIQUE ARTICLE
-    title = browser.find_element("id", "jform_title")
-    alias = browser.find_element("id", "jform_alias")
+        # browser.maximize_window()
+        # browser.get('https://joomla4.master-geomatique.org/administrator/index.php?option=com_content&view=article&layout=edit')
+        browser.get('https://j4binv.master-geomatique.org/administrator/index.php?option=com_content&view=article&layout=edit')
 
-    title.send_keys(xml_title)
-    alias.send_keys(xml_alias)
+        # CHAMPS CLASSIQUE ARTICLE
+        title = browser.find_element("id", "jform_title")
+        alias = browser.find_element("id", "jform_alias")
 
-    browser.find_element("id", "save-group-children-save").click()
+        title.send_keys(xml_title)
+        alias.send_keys(xml_alias)
 
-    time.sleep(1)
+        browser.find_element("id", "save-group-children-save").click()
+
+        time.sleep(1)
--- ArticlesEditer.py
+++ ArticlesEditer.py
@@ -30,19 +30,39 @@ browser.find_element("xpath", "//select/option[@value='0']").click()
 
 for my_poi in root.findall('Tmp_extract_alias_joo_content'):
     xml_title = my_poi.find('title').text
+    xml_title = xml_title.replace('  ', ' ')
 
-    print(xml_title)
-    # browser.maximize_window()
+    if xml_title != 'Test georges' \
+            and xml_title != 'Test cours m2 georges' \
+            and xml_title != 'Test georges4' \
+            and xml_title != 'Logo (New)' \
+            and xml_title != 'Présentation générale' \
+            and xml_title != 'Options d&apos;édition' \
+            and xml_title != 'Options d\'édition' \
+            and xml_title != 'Précisions' \
+            and xml_title != 'Bienvenue' \
+            and xml_title != 'IMG1' \
+            and xml_title != 'IMG2' \
+            and xml_title != 'IMG3' \
+            and xml_title != 'Présentation Actualités' \
+            and xml_title != 'Présentation Actualités 2' \
+            and xml_title != 'Master Géomatique' \
+            and xml_title != 'Expertise numérique et territoriale' \
+            and xml_title != 'Formation en alternance' \
+            and xml_title != 'XXXXXXXXX':
+
+        print(xml_title)
+        # browser.maximize_window()
 
 
-    # browser.find_element("xpath", "//a[@href='/administrator/index.php?option=com_content&task=article.edit&id=1']").click()
+        # browser.find_element("xpath", "//a[@href='/administrator/index.php?option=com_content&task=article.edit&id=1']").click()
 
-    # browser.find_element(By.LINK_TEXT, "Présentation générale").click()
+        # browser.find_element(By.LINK_TEXT, "Présentation générale").click()
 
-    element = browser.find_element(By.LINK_TEXT, xml_title)
-    time.sleep(1)
-    browser.execute_script("arguments[0].click();", element)
-    time.sleep(1)
-    browser.find_element("id", "save-group-children-save").click()
+        element = browser.find_element(By.LINK_TEXT, xml_title)
+        time.sleep(0.5)
+        browser.execute_script("arguments[0].click();", element)
+        time.sleep(0.5)
+        browser.find_element("id", "save-group-children-save").click()
 
-    time.sleep(1)
+        time.sleep(0.5)
